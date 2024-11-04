afc1dc41022845dc0c94c73e0e2b4821df1cde99
TukuNoni71 <Adonisjzepeda@gmail.com>
2024-11-03 20:35:53 -0500

    small changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,36 @@
 // import { useState } from 'react'
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Profile from './components/Profile';
 import Favorites from './components/Favorites';
 import Footer from './components/Footer';
+import Landing from './pages/Landing';
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-        <Header />
-        <Profile />
-        <Favorites />
-        <Footer />
-    </>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/Favorites" element={<Favorites />} />
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/contact" element={<Contact />} /> */}
+        {/* Add other routes as needed */}
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
 export default App;
+
+{/* <>
+  <Header />
+  <Profile />
+  <Favorites />
+  <Footer />
+</> */}
